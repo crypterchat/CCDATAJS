e8f10a7578c47d45466d6397487c6fdb3547f3a8
Mr Erickson <erickson.poel@gmail.com>
2021-06-09 19:22:51 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,4 @@
-CRPN,
-  
-  
+
   {
   "data": {
     "ethereum": {
@@ -23,3 +21,37 @@ CRPN,
     }
   }
 }
+
+
+query ($network: EthereumNetwork!,
+                              $token: String!,
+                              $from: ISO8601DateTime,
+                              $till: ISO8601DateTime){
+                          ethereum(network: $network){
+                            transfers(currency: {is: $token}
+                            amount: {gt: 0}
+                            date: {since: $from till: $till}
+                            ){
+
+                              currency{
+                                symbol
+                              }
+
+                              median: amount(calculate: median)
+                              average: amount(calculate: average)
+
+                              amount
+                              count
+
+                              days: count(uniq: dates)
+
+                              sender_count: count(uniq: senders)
+                              receiver_count: count(uniq: receivers)
+
+                              min_date:minimum(of: date)
+                              max_date:maximum(of: date)
+                            }
+                          }
+                        }
+                        
+                        {"limit":10,"offset":0,"network":"bsc","token":"0x84b2537ef5a872a7c67fb03c6cefc51dd4be3c68"}
